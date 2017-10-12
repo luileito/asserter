dc14e5a46623cbb397bb4673a615fe7ee0863622
Luis Leiva <lleiva@sciling.com>
2017-10-12 22:45:44 +0200

    Initial files

---
+++ assert.js
@@ -0,0 +1,128 @@
+(function() {
+  // Detect env.
+  var isNode = typeof module !== 'undefined' && module.exports;
+  /**
+    A tiny assertion lib for both nodejs and the browser.
+    @module assert
+    @author Luis Leiva
+    @example
+  */
+  var asserter = {
+    tests: [],
+    currentTest: null,
+    negated: false,
+    test: function(message) {
+      this.currentTest = message;
+      this.negated = false;
+      return this;
+    },
+    _set: function(value, reference, operator, result) {
+      this.tests.push({
+        value: value,
+        reference: reference,
+        operator: operator,
+        negated: this.negated,
+        result: result,
+        message: this.currentTest,
+      });
+      return this;
+    },
+    _condition: function(expr) {
+      return !this.negated ? expr : !expr;
+    },
+    not: function() {
+      this.negated = true;
+      return this;
+    },
+    // BEGIN General-purpose matchers.
+    equals: function(value, reference) {
+      return this._set(value, reference, '==', this._condition(value == reference));
+    },
+    strictEquals: function(value, reference) {
+      return this._set(value, reference, '===', this._condition(value === reference));
+    },
+    // BEGIN Numberical matchers.
+    isGreaterThan: function(value, reference) {
+      return this._set(value, reference, '>', this._condition(value > reference));
+    },
+    isGreaterThanOrEquals: function(value, reference) {
+      return this._set(value, reference, '>=', this._condition(value >= reference));
+    },
+    isLessThan: function(value, reference) {
+      return this._set(value, reference, '<', this._condition(value < reference));
+    },
+    isLessThanOrEquals: function(value, reference) {
+      return this._set(value, reference, '<=', this._condition(value <= reference));
+    },
+    // BEGIN RegExp matchers.
+    matches: function(value, reference) {
+      return this._set(value, reference, 'matches', this._condition(value.test(reference)));
+    },
+    // BEGIN String matchers.
+    contains: function(value, reference) {
+      return this._set(value, reference, 'contains', this._condition(value.indexOf(reference) > -1));
+    },
+    // BEGIN Other matchers.
+    throws: function(fn) {
+      try {
+        fn();
+        this._set('success', 'error', 'is', this._condition(false));
+      } catch(err) {
+        this._set('success', 'success', 'is', this._condition(true));
+      }
+    },
+    display: console.log,
+    // Main.
+    run: function() {
+      var successes = 0;
+      var errors = 0;
+      this.tests.forEach(function(test) {
+        var label = test.result ? greenBg('PASS') : redBg('FAIL');
+        if (!test.result) {
+          var reason = test.negated
+                     ? sprintf('was NOT expecting %s %s %s', test.reference, test.operator, test.value)
+                     : sprintf('was expecting %s %s %s', test.reference, test.operator, test.value);
+          test.message += ' ' + yellow(reason);
+          errors++;
+        } else {
+          successes++;
+        }
+        asserter.display(label, test.message);
+      });
+      asserter.display('\n%s/%s tests passed (%s errors).', successes, successes + errors, errors);
+    },
+  };
+
+  // How is that this function is not part of the std lib?
+  function sprintf() {
+    var args = [].slice.call(arguments);
+    var str = args.shift();
+    args.forEach(function(arg) {
+      str = str.replace('%s', arg);
+    });
+    return str;
+  }
+
+  // Color helpers stuff.
+  function colorize(color, msg) {
+    if (!isNode) return sprintf('[%s]', msg);
+    return color + msg + '\x1b[0m';
+  }
+  function redBg(msg) {
+    return colorize('\x1b[41m', msg);
+  }
+  function greenBg(msg) {
+    return colorize('\x1b[42m', msg);
+  }
+  function yellow(msg) {
+    return colorize('\x1b[33m', msg);
+  }
+
+  // Expose library.
+  if (isNode) {
+    module.exports = asserter;
+  } else {
+    window.asserter = asserter;
+  }
+
+})();
