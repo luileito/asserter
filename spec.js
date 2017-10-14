7d5b7063a2d8bee46ee982a85298606e986da5f2
Luis Leiva <lleiva@sciling.com>
2017-10-14 13:43:25 +0200

    Updated tests coverage

--- spec.js
+++ spec.js
@@ -1,15 +1,20 @@
 describe('asserter', function() {
 
   var asserter = require('./asserter');
-  // Suppress tests output.
+  // Suppress output.
   asserter.display = () => {};
 
+  var testSuite = require('./test');
   var hasFailed = (test) => !test.result;
 
   it('should pass default tests', function() {
     // Run default tests.
-    var testSuite = require('./test');
     expect(testSuite.tests.every(hasFailed)).toBe(false);
+  });
+
+  it('should clean tests after run', function() {
+    testSuite.run();
+    expect(testSuite.tests.length).toBe(0);
   });
 
   it('should fail test', function() {
